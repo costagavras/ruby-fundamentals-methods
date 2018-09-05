843f64287cc399ef621ff60766ebd7456c49c60c
costagavras <cgavras@yahoo.ca>
2018-09-05 14:52:52 -0400

    ex1

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+# Define a method called double that accepts an argument called my_number and returns that number multiplied by 2.
+
+def double(my_number)
+  return my_number * 2
+end
+
+a_num = -3.458930
+mult_by_2 = double(a_num)
+puts mult_by_2
